04afc9140c63e20716ce3bb2006f44ef5fddb5bb
Alexey Mitin <mitin.a@mascot.games>
2025-05-06 12:19:32 +0300

    update configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,12 @@
-// webpack.config.js
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './public',
-    port: 3000,
-    open: true,
-    hot: true
+    clean: true // Очищает dist перед сборкой
   },
   resolve: {
     extensions: ['.ts', '.js']
@@ -24,7 +17,28 @@ module.exports = {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource'
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html' // Использует ваш HTML-шаблон
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: 9000
+  },
+  devtool: 'source-map',
+  optimization: {
+    splitChunks: false
+  },
+  performance: { hints: false }
 };
